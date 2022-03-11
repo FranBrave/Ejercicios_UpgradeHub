d8cb466f6eb5f38316e8912e06a3069a163ded1f
FranBrave <valiente.torrente@bootcamp-upgrade.com>
2022-03-11 15:14:54 +0100

    Add iteración3.js

---
+++ JS/Basic_Algorithms/Iteración3.js
@@ -0,0 +1,36 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+let numA = 10;
+let numB = 5;
+
+console.log(numA/numB);
+
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+numA = 10;
+numB = 2;
+
+console.log(numA/numB);
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+console.log(15/9);
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+
+let y = 10;
+let z = 5;
+let x = y+z;
+
+console.log(x);
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+
+y = 10;
+z = 5;
+x = y*z;
+
+console.log(x);
